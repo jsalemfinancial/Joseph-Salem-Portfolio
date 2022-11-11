187c607d5ac6d19a16b9963590c396b7257eb46f
jsalemfinancial <jsalemfinancial@gmail.com>
2022-11-11 12:35:24 -0500

    Organized File Structures and Updated Interest Tab.
    - Added content to Interests tab.
    - Split scripts and styles to be more manageable.
    - Updated some scripting.

---
+++ js/listeners.js
@@ -0,0 +1,66 @@
+// Event Listeners
+
+window.addEventListener("load", function() {
+    document.getElementById("loading").style.display= "none";
+    
+    document.getElementsByClassName("navbar-button")[0].style.boxShadow = "0 -0.5vh 0 #b8bb26 inset"; //Initial selected tab.
+    TabSelector.pageSelect(0);
+
+    window.addEventListener("scroll", function() {
+        const topPageButtonKeyframes = [
+            {opacity: 0}, 
+            {opacity: 1}
+        ];
+
+        var pageHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, 
+            document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ); //Thanks StackExchange.
+
+        if ((window.scrollY || document.documentElement.scrollTop) > (pageHeight * 0.1) && (document.getElementById("top-page-button").style.display == "")) {
+            document.getElementById("top-page-button").animate(topPageButtonKeyframes, {duration: 500, fill: "forwards"});
+            document.getElementById("top-page-button").style.display = "flex";
+            
+        } else if ((window.scrollY || document.documentElement.scrollTop) < (pageHeight * 0.1) && (document.getElementById("top-page-button").style.display == "flex")) {
+            document.getElementById("top-page-button").style.display = "";
+        };
+    });
+
+    document.getElementById("top-page-button").addEventListener("click", function() {
+        window.scrollY = 0;
+        document.documentElement.scrollTop = 0;
+    });
+
+    document.querySelectorAll(".navbar-button").forEach(tab => {
+        tab.addEventListener("click", function() {
+            for (var i = 0; i < document.getElementsByClassName("navbar-button").length; i++) {
+                if (document.getElementsByClassName("navbar-button")[i].innerText == tab.innerText) {
+                    document.getElementsByClassName("navbar-button")[i].style.boxShadow = "0 -0.5vh 0 #b8bb26 inset";
+                    TabSelector.pageSelect(i);
+                } else {
+                    document.getElementsByClassName("navbar-button")[i].style.boxShadow = "0 -0.5vh 0 #fabd2f inset";
+                };
+            };
+        });
+    });
+
+    document.querySelectorAll(".chart-button").forEach(overlay => {
+        overlay.addEventListener("click", function(event) {
+            console.log("click!");
+            for (var i = 0; i < document.getElementsByClassName("chart-button").length; i++) {
+                if (document.getElementsByClassName("chart-button")[i] == event.target) {
+                    Chart.loadChartLightbox(i);
+                };
+            };
+        });
+    });
+
+    document.querySelectorAll(".lightbox-close-button").forEach(overlay => {
+        overlay.addEventListener("click", function(event) {
+            console.log("click!");
+            for (var i = 0; i < document.getElementsByClassName("lightbox-close-button").length; i++) {
+                if (document.getElementsByClassName("lightbox-close-button")[i] == event.target) {
+                    Chart.unloadChartLightbox(i);
+                };
+            };
+        });
+    });
+});
---
+++ js/page_classes.js
@@ -0,0 +1,50 @@
+let TabSelector = new class TabSelector {
+    constructor() {
+        this.tabContent = document.getElementById("main-content-box").getElementsByClassName("main-content-elements");
+        this.numTabs = document.getElementsByClassName("navbar-button").length;
+    }
+
+    pageSelect(pageNum) {
+        for (var i = 0; i < this.numTabs; i++) {
+            if (i == pageNum) {
+                this.tabContent[i].style.display = "flex";
+            } else {
+                this.tabContent[i].style.display = "none";
+            };
+        };
+    }
+}
+
+let Chart = new class Chart {
+    constructor() {
+        this.currentLightbox = document.getElementsByClassName("chart-lightbox");
+    }
+    
+    loadChartLightbox(index) {
+        this.currentLightbox[index].style.display = "flex";
+        document.getElementsByTagName("body")[0].style.overflow = "hidden";
+    }
+
+    unloadChartLightbox(index) {
+        this.currentLightbox[index].style.display = "none";
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
+    }
+}
+
+let MusicList = new class MusicList {
+    constructor() {
+        
+    }
+}
+
+let FoodGrid = new class FoodGrid {
+    constructor() {
+        
+    }
+}
+
+let GameReview = new class GameReview {
+    constructor() {
+        
+    }
+}
